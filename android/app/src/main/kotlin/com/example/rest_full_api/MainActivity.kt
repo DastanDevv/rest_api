7168eff3f810fa91b96335f12acc528c8e8202c9
DastanDevv <dastanalmaz6eko1ch@gmail.com>
2024-09-10 11:11:39 +0600

    firts commit

---
+++ android/app/src/main/kotlin/com/example/rest_full_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rest_full_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:rest_full_api/app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
